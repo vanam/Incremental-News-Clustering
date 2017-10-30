6043f7f943c0cf4de37c0d34ba2d82e47cb13bab
Martin Váňa <vanam@students.zcu.cz>
2017-10-30 21:05:49 +0100

    Add measures - Mutual Information, Normalized Mutual Information and V-Measure

--- clustering/measures.py
+++ clustering/measures.py
@@ -1,6 +1,8 @@
 from collections import Counter
 
 import numpy as np
+import pandas as pd
+from pandas import crosstab
 from scipy.special import comb
 
 
@@ -84,6 +86,143 @@ def rand_index(clusters, classes):
     return (tp + tn) / c2N
 
 
+def mutual_information(clusters, classes, contingency=None):
+    if contingency is None:
+        contingency = crosstab(clusters, classes, rownames=['clusters'], colnames=['classes'], margins=True)
+
+    # Number of data points
+    N = len(clusters)
+
+    if N == 0:
+        return 0.0
+
+    # Find cluster labels
+    cluster_labels = np.unique(clusters)
+
+    # Find class labels
+    class_labels = np.unique(classes)
+
+    mutual_information = 0.0
+
+    for kl in cluster_labels:
+        for cl in class_labels:
+            a_ij = contingency[cl][kl]
+
+            if a_ij == 0.0:
+                continue
+
+            mutual_information += a_ij / N * (np.log(a_ij * N) - np.log(contingency['All'][kl] * contingency[cl]['All']))
+
+    return mutual_information
+
+
+def normalized_mutual_information(clusters, classes):
+    I = mutual_information(clusters, classes)
+    entropy_K, entropy_C = entropy(clusters), entropy(classes)
+
+    nmi = I / np.sqrt(entropy_K * entropy_C)
+
+    return nmi
+
+
+def normalized_mutual_information2(clusters, classes):
+    """
+    See (25.12) in Murphy, p. 134.
+    """
+    I = mutual_information(clusters, classes)
+    entropy_K, entropy_C = entropy(clusters), entropy(classes)
+
+    nmi = I / ((entropy_K + entropy_C) / 2)
+
+    return nmi
+
+
+def homogeneity(clusters, classes):
+    return _homogeneity_completeness_v_measure(clusters, classes)[0]
+
+
+def completeness(clusters, classes):
+    return _homogeneity_completeness_v_measure(clusters, classes)[1]
+
+
+def v_measure(clusters, classes):
+    return _homogeneity_completeness_v_measure(clusters, classes)[2]
+
+
+def _homogeneity_completeness_v_measure(clusters, classes):
+    """
+    https://www.researchgate.net/publication/221012656_V-Measure_A_Conditional_Entropy-Based_External_Cluster_Evaluation_Measure
+    https://github.com/scikit-learn/scikit-learn/blob/f3320a6f/sklearn/metrics/cluster/supervised.py#L217
+    """
+    # Calculate contingency table
+    A = crosstab(clusters, classes, rownames=['clusters'], colnames=['classes'], margins=True)
+
+    # Calculate entropy - H(C) - of class labeling
+    # entropy_C = -np.sum([A[l]['All'] / N * np.log(A[l]['All'] / N) for l in class_labels])
+    entropy_C = entropy(classes)
+
+    # Calculate entropy - H(K) - of cluster labeling
+    # entropy_K = -np.sum([A['All'][l] / N * np.log(A['All'][l] / N) for l in cluster_labels])
+    entropy_K = entropy(clusters)
+
+    # # Number of data points
+    # N = len(clusters)
+    #
+    # # Find cluster labels
+    # cluster_labels = np.unique(clusters)
+    #
+    # # Find class labels
+    # class_labels = np.unique(classes)
+    #
+    # # Calculate conditional entropy H(K|C)
+    # entropy_C_K = -np.sum([np.sum([A[cl][kl] / N * np.log(A[cl][kl] / A['All'][kl]) for cl in class_labels if A[cl][kl] != 0.0]) for kl in cluster_labels])
+    #
+    # # Calculate conditional entropy H(C|K)
+    # entropy_K_C = -np.sum([np.sum([A[cl][kl] / N * np.log(A[cl][kl] / A[cl]['All']) for kl in cluster_labels if A[cl][kl] != 0.0]) for cl in class_labels])
+
+    # Mutual information
+    # I(C; K) = H(C) − H(C|K) = H(K) − H(K|C)
+    # I = entropy_C - entropy_C_K
+    # I = entropy_K - entropy_K_C
+    I = mutual_information(clusters, classes, contingency=A)
+
+    # 1 - H(C|K) / H(C) = I / H(C)
+    # homogeneity = 1 - entropy_C_K / entropy_C if entropy_C else 1.0
+    homogeneity = I / entropy_C if entropy_C else 1.0
+    # 1 - H(K|C) / H(K) = I / H(K)
+    # completeness = 1 - entropy_K_C / entropy_K if entropy_K else 1.0
+    completeness = I / entropy_K if entropy_K else 1.0
+
+    if homogeneity + completeness == 0.0:
+        v_measure = 0.0
+    else:
+        v_measure = 2 * (homogeneity * completeness) / (homogeneity + completeness)
+
+    return homogeneity, completeness, v_measure
+
+
+def entropy(labels):
+    """
+    Calculate entropy using maximum likelihood estimates for label probabilities.
+    """
+    # Length of an array
+    N = len(labels)
+
+    if N == 0:
+        return 0.0
+
+    # Convert labels to natural numbers
+    label_idx = np.unique(labels, return_inverse=True)[1]
+
+    # Count frequency of labels
+    pi = np.bincount(label_idx).astype(np.float64)
+
+    # Keep only non-zero ones
+    pi = pi[pi > 0]
+
+    # log(a / b) calculated as log(a) - log(b)
+    return -np.sum((pi / N) * (np.log(pi) - np.log(N)))
+
 
 if __name__ == "__main__":
     #  Based on Figure 16.4 of (Manning et al. 2008)
@@ -123,6 +262,14 @@ if __name__ == "__main__":
         3, 3, 3,
     ])
 
-    print("Purity:     %f" % purity(clusters, classes))
-    print("Purity2:    %s" % purity2(clusters, classes))
-    print("Rand index: %f" % rand_index(clusters, classes))
+    print("Purity:                          %f" % purity(clusters, classes))
+    print("Purity2:                         %s" % purity2(clusters, classes))
+    print("Rand index:                      %f" % rand_index(clusters, classes))
+    print("Entropy (clusters):              %f" % entropy(clusters))
+    print("Entropy (classes):               %f" % entropy(classes))
+    print("Homogeneity:                     %f" % homogeneity(clusters, classes))
+    print("Completeness:                    %f" % completeness(clusters, classes))
+    print("V-Measure:                       %f" % v_measure(clusters, classes))
+    print("Mutual Information:              %f" % mutual_information(clusters, classes))
+    print("Normalized Mutual Information:   %f" % normalized_mutual_information(clusters, classes))
+    print("Normalized Mutual Information 2: %f" % normalized_mutual_information2(clusters, classes))
