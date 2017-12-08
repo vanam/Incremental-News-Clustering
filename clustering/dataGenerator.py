8815221bbd9c4d287d3925437b9a90f1860ad009
Martin Váňa <vanam@students.zcu.cz>
2017-12-08 10:44:09 +0100

    Add random clusters generator.

--- clustering/dataGenerator.py
+++ clustering/dataGenerator.py
@@ -3,11 +3,36 @@ import ast
 import configparser
 
 import numpy as np
+from scipy.stats import wishart
 
 KEYS = ['mean', 'cov', 'size']
 
+CLUSTER_DIM_RANGE = [50, 150]
 
-def main(config_file, output_file):
+
+def main(config_file, output_file, random=False, k=20, d=2):
+    if random:
+        # Mean hyper-parameters
+        mean = np.zeros(d)
+        cov = np.eye(d) * k * 8
+        scale = np.eye(d)
+
+        # Define config
+        config = configparser.ConfigParser()
+
+        for i in range(k):
+            config['c%d' % i] = {}
+            cluster_config = config['c%d' % i]
+            cluster_config['mean'] = str(np.random.multivariate_normal(mean, cov, 1)[0].tolist())
+            cluster_config['cov'] = str(wishart.rvs(d + 1, scale).tolist())
+            cluster_config['size'] = str(np.random.randint(CLUSTER_DIM_RANGE[0], CLUSTER_DIM_RANGE[1]))
+
+        # Write config
+        config.write(config_file)
+
+        # Move to the beginning of a file
+        config_file.seek(0, 0)
+
     # Read config
     config = configparser.ConfigParser()
     config.read_file(config_file)
@@ -37,8 +62,8 @@ def main(config_file, output_file):
 
 
 def is_valid_config(string):
-    # Check if file exists and is readable
-    file = argparse.FileType('r')(string)
+    # Check if file exists and is writeable
+    file = argparse.FileType('w+')(string)
 
     # Check if file is a configuration file
     config = configparser.ConfigParser()
@@ -65,6 +90,13 @@ if __name__ == "__main__":
                         help='a data configuration file')
     parser.add_argument('output', metavar='output-file', type=argparse.FileType('wb'),
                         help='an output data file')
+    parser.add_argument('-k, --clusters', dest='k', type=int,
+                        help='the number of clusters (default: 10)')
+    parser.add_argument('-d, --dimension', dest='d', type=int,
+                        help='the data dimension (default: 2)')
+    parser.add_argument('-r, --random', dest='random', action='store_true',
+                        help='randomly generate data')
+    parser.set_defaults(k=10, d=2, random=False)
 
     args = parser.parse_args()
-    main(args.config, args.output)
+    main(args.config, args.output, args.random, args.k)
