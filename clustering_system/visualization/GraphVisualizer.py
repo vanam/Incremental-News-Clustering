9e1ff6be88d04d1e4394de86dbfdbfe5cd886483
Martin Váňa <vanam@students.zcu.cz>
2018-04-23 16:39:24 +0200

    Add visualizer comments.

--- clustering_system/visualization/GraphVisualizer.py
+++ clustering_system/visualization/GraphVisualizer.py
@@ -4,11 +4,19 @@ from networkx import DiGraph, write_gexf
 
 
 class GraphVisualizer:
+    """Visualize clustering dynamics as a graph."""
 
     def __init__(self):
         self.documents = {}
 
     def add_documents(self, docs: list, metadata: list, time: int):
+        """
+        Add documents to a graph.
+
+        :param docs: The document list
+        :param metadata: The document metadata
+        :param time: The time of visualization
+        """
         for doc, metadata in zip(docs, metadata):
             doc_id = metadata[0]
             title = metadata[2]
@@ -16,6 +24,15 @@ class GraphVisualizer:
             self.documents[str(doc_id)] = [time, time, doc, [], title, defaultdict(list)]
 
     def set_cluster_for_doc(self, t, doc_id, cluster_id, linked_doc_id: int = None):
+        """
+        Add document visualization attributes at time t.
+
+        :param t: The time of visualization
+        :param doc_id: The document id
+        :param cluster_id: The cluster id
+        :param linked_doc_id: The link to another document
+        :return:
+        """
         self.documents[doc_id][1] = t + 1
         self.documents[doc_id][3].append((int(cluster_id), t, t + 1))
 
@@ -23,6 +40,11 @@ class GraphVisualizer:
             self.documents[doc_id][5][linked_doc_id].append((t, t + 1))
 
     def save(self, filename: str):
+        """
+        Save graph to GEXF file.
+
+        :param filename: The filename
+        """
         graph = DiGraph(mode="dynamic")
 
         for doc_id, doc in self.documents.items():
--- clustering_system/visualization/LikelihoodVisualizer.py
+++ clustering_system/visualization/LikelihoodVisualizer.py
@@ -3,16 +3,28 @@ import numpy as np
 
 
 class LikelihoodVisualizer:
+    """Visualize clustering likelihood during sampling."""
 
     def __init__(self):
         self.likelihood = []
         self.N = []
 
     def add(self, likelihood: float, N: int):
+        """
+        Add likelihood and the number of observations.
+
+        :param likelihood: The log likelihood
+        :param N: the number of observations
+        """
         self.likelihood.append(likelihood)
         self.N.append(N)
 
     def save(self, filename: str):
+        """
+        Save chart containing the likelihood over time.
+
+        :param filename: The filename
+        """
         t = len(self.likelihood)
 
         # x axis is time
