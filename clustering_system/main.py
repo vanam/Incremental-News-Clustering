6d376152fe35672ce1335cd40b2bf6297ef20b0c
Martin Váňa <vanam@students.zcu.cz>
2018-05-09 22:04:41 +0200

    Remove artificial data and related scripts.

--- clustering_system/main.py
+++ clustering_system/main.py
@@ -15,21 +15,16 @@ from sklearn.decomposition import IncrementalPCA
 from clustering_system.clustering.DummyClustering import DummyClustering
 from clustering_system.clustering.bgmm.BgmmClustering import BgmmClustering
 from clustering_system.clustering.igmm.CrpClustering import CrpClustering
-from clustering_system.clustering.igmm.DdCrpClustering import DdCrpClustering, logistic_decay, exponential_decay
+from clustering_system.clustering.igmm.DdCrpClustering import DdCrpClustering, logistic_decay
 from clustering_system.clustering.mixture.GaussianMixtureABC import NormalInverseWishartPrior
-from clustering_system.corpus.ArtificialCorpus import ArtificialCorpus
 from clustering_system.corpus.FolderAggregatedBowNewsCorpora import FolderAggregatedBowNewsCorpora
 from clustering_system.corpus.FolderAggregatedLineNewsCorpora import FolderAggregatedLineNewsCorpora
 from clustering_system.corpus.LineCorpus import LineCorpus
 from clustering_system.corpus.SinglePassCorpusWrapper import SinglePassCorpusWrapper
-from clustering_system.corpus.SingletonCorpora import SingletonCorpora
-from clustering_system.evaluator.RandomEvaluator import RandomEvaluator
 from clustering_system.model.Doc2vec import Doc2vec
-from clustering_system.model.Identity import Identity
 from clustering_system.model.Lda import Lda
 from clustering_system.model.Lsa import Lsa
 from clustering_system.model.Random import Random
-from clustering_system.visualization.ClusterVisualizer import ClusterVisualizer
 from clustering_system.visualization.GraphVisualizer import GraphVisualizer
 from clustering_system.visualization.LikelihoodVisualizer import LikelihoodVisualizer
 from evaluator.Evaluator import Evaluator
@@ -38,12 +33,10 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 
 class Corpus(Enum):
-    artificial = 0
     news = 1
 
 
 class Model(Enum):
-    identity = 0
     random = 1
     LSA = 2
     LDA = 3
@@ -72,7 +65,7 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--size', type=int, choices=[10, 50, 100], help='the size of a feature vector (if applicable)')
     parser.add_argument('-S', type=float, help='the scale of the diagonal prior S_0')
     parser.add_argument('-t', '--test', dest='test', action='store_true', help='use test data')
-    parser.set_defaults(corpus=Corpus.artificial.name, model=Model.identity.name, clustering=Clustering.ddCRP.name,
+    parser.set_defaults(corpus=Corpus.news.name, model=Model.random.name, clustering=Clustering.ddCRP.name,
                         K=2, size=100, test=False, seed=False, i=0, n=20, a=0.01, b=0, k=0.01, S=1)
     args = parser.parse_args()
 
@@ -80,17 +73,6 @@ if __name__ == "__main__":
         args.corpus = Corpus[args.corpus]
         args.model = Model[args.model]
         args.clustering = Clustering[args.clustering]
-
-        corpus = args.corpus
-        model = args.model
-
-        if corpus == Corpus.artificial and model != Model.identity:
-            logging.error("Only identity model can be used with artificial corpus.")
-            return False
-
-        elif corpus == Corpus.news and model == Model.identity:
-            logging.error("The identity model cannot be used with news corpus.")
-            return False
 
         return True
 
@@ -140,7 +122,6 @@ if __name__ == "__main__":
     training_dir = os.path.join(data_dir, "genuine", "training")
     heldout_dir = os.path.join(data_dir, "genuine", "heldout", "2017", "10")
     test_dir = os.path.join(data_dir, "genuine", "test", "2017", "10")
-    artificial_file = os.path.join(data_dir, "artificial", "02.dat")
     graph_visualization_file = os.path.join(temp_visualization_dir, 'visualization.gexf')
     likelihood_visualization_file = os.path.join(temp_visualization_dir, 'clustering_likelihood.png')
 
@@ -155,63 +136,56 @@ if __name__ == "__main__":
     ########################
 
     # Select corpora and model
-    if corpus_type == Corpus.artificial:
-        # For artificial data initialize identity model
-        training_corpus = ArtificialCorpus(input=artificial_file)
-        size = training_corpus.size
-        corpora = SingletonCorpora(ArtificialCorpus(input=artificial_file, metadata=True))
-        model = Identity()
+    if model_type in [Model.random, Model.LSA, Model.LDA]:
+        # Load BoW corpus and dictionary from temp files
+        dictionary = Dictionary.load(dictionary_file)
+        training_corpus = MmCorpus(training_mm_corpus_file)
+
+        # Initialize correct model
+        if model_type == Model.random:
+            model = Random(size=size)
+        elif model_type == Model.LSA:
+            model_file = os.path.join(training_dir, 'model_%d.lsa' % size)
+            tfidf_file = os.path.join(training_dir, 'model_%d.lsa.tfidf' % size)
+
+            model = Lsa(dictionary, size=size, lsa_filename=model_file, tfidf_filename=tfidf_file)
+        elif model_type == Model.LDA:
+            model_file = os.path.join(training_dir, 'model_%d.lda' % size)
+
+            model = Lda(dictionary, size=size, lda_filename=model_file)
+        else:
+            logging.error("Unknown model type '%s'" % model_type)
+            sys.exit(1)
     else:
-        if model_type in [Model.random, Model.LSA, Model.LDA]:
-            # Load BoW corpus and dictionary from temp files
-            dictionary = Dictionary.load(dictionary_file)
-            training_corpus = MmCorpus(training_mm_corpus_file)
+        # Load LoW corpus and dictionary from files
+        dictionary = Dictionary.load(dictionary_file)
+        training_corpus = LineCorpus(training_low_corpus_file)
 
-            # Initialize correct model
-            if model_type == Model.random:
-                model = Random(size=size)
-            elif model_type == Model.LSA:
-                model_file = os.path.join(training_dir, 'model_%d.lsa' % size)
-                tfidf_file = os.path.join(training_dir, 'model_%d.lsa.tfidf' % size)
+        model_file = os.path.join(training_dir, 'model_%d.d2v' % size)
 
-                model = Lsa(dictionary, size=size, lsa_filename=model_file, tfidf_filename=tfidf_file)
-            elif model_type == Model.LDA:
-                model_file = os.path.join(training_dir, 'model_%d.lda' % size)
+        model = Doc2vec(size=size, d2v_filename=model_file)
 
-                model = Lda(dictionary, size=size, lda_filename=model_file)
-            else:
-                logging.error("Unknown model type '%s'" % model_type)
-                sys.exit(1)
-        else:
-            # Load LoW corpus and dictionary from files
-            dictionary = Dictionary.load(dictionary_file)
-            training_corpus = LineCorpus(training_low_corpus_file)
+    # Load test corpora
+    sep_t = None
 
-            model_file = os.path.join(training_dir, 'model_%d.d2v' % size)
+    if model_type == Model.doc2vec:
+        # Heldout LoW corpora
+        corpora = FolderAggregatedLineNewsCorpora(heldout_dir, temp_dir, dictionary, language=language)
 
-            model = Doc2vec(size=size, d2v_filename=model_file)
+        # Test LoW corpora
+        if args.test:
+            sep_t = len(corpora)
+            test_corpora = FolderAggregatedLineNewsCorpora(test_dir, temp_dir, dictionary, language=language)
+            corpora = itertools.chain(corpora, test_corpora)
+    else:
+        # Heldout BoW corpora
+        corpora = FolderAggregatedBowNewsCorpora(heldout_dir, temp_dir, dictionary, language=language)
 
-        # Load test corpora
-        sep_t = None
-
-        if model_type == Model.doc2vec:
-            # Heldout LoW corpora
-            corpora = FolderAggregatedLineNewsCorpora(heldout_dir, temp_dir, dictionary, language=language)
-
-            # Test LoW corpora
-            if args.test:
-                sep_t = len(corpora)
-                test_corpora = FolderAggregatedLineNewsCorpora(test_dir, temp_dir, dictionary, language=language)
-                corpora = itertools.chain(corpora, test_corpora)
-        else:
-            # Heldout BoW corpora
-            corpora = FolderAggregatedBowNewsCorpora(heldout_dir, temp_dir, dictionary, language=language)
-
-            # Test BoW corpora
-            if args.test:
-                sep_t = len(corpora)
-                test_corpora = FolderAggregatedBowNewsCorpora(test_dir, temp_dir, dictionary, language=language)
-                corpora = itertools.chain(corpora, test_corpora)
+        # Test BoW corpora
+        if args.test:
+            sep_t = len(corpora)
+            test_corpora = FolderAggregatedBowNewsCorpora(test_dir, temp_dir, dictionary, language=language)
+            corpora = itertools.chain(corpora, test_corpora)
 
     # Make sure we can see data only once
     corpora = SinglePassCorpusWrapper(corpora)
@@ -238,15 +212,6 @@ if __name__ == "__main__":
         def f(d: float):
             return logistic_decay(d, args.b)
 
-        if corpus_type == Corpus.artificial:
-            # Decay function for artificial data
-            b = 5
-
-            def f(d):
-                # Hack distance to look like time
-                d = np.math.hypot(d[0], d[1]) * 60 * 60 * 24
-                return logistic_decay(d, args.b)
-
         clustering = DdCrpClustering(K, size, args.a, prior, args.n, f, visualizer=likelihood_visualizer)
     else:
         logging.error("Unknown clustering algorithm '%s'" % clustering_type)
@@ -265,10 +230,7 @@ if __name__ == "__main__":
 
     # Init evaluator
     logging.info("Initializing evaluator...")
-    if corpus_type == Corpus.artificial:
-        evaluator = RandomEvaluator(K)
-    else:
-        evaluator = Evaluator(ground_truth_file, language=language)
+    evaluator = Evaluator(ground_truth_file, language=language)
 
     # Iterate over heldout/test corpora
     for t, docs_metadata in enumerate(corpora):
@@ -292,7 +254,7 @@ if __name__ == "__main__":
 
         # Reduce vector dimension for visualization
         ipca.partial_fit(docs)
-        reduced_docs = ipca.transform(docs) if corpus_type == Corpus.artificial else docs
+        reduced_docs = ipca.transform(docs)
 
         # Visualization
         graph_visualizer.add_documents(reduced_docs, metadata, t)
